2574c2b116ce562212df25cb5272964d430df3ed
Stefano Valentini <valentini.stefano@outlook.com>
2019-08-27 17:14:47 +0200

    fixed pathname

--- spark-submit.sh
+++ spark-submit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SPARK_HOME="/mnt/e/Library/spark-2.4.3-bin-hadoop2.7"
-JAR_PATH="/mnt/e/Projects/big-data-project/twitter-spark-consumer/target/twitterSparkConsumer-1.0-SNAPSHOT.jar"
+JAR_PATH="/mnt/e/Projects/twitter-profanity-filter/twitter-spark-consumer/target/twitterSparkConsumer-1.0-SNAPSHOT.jar"
 
 # run the spark master server
 $SPARK_HOME/bin/spark-submit \
